ddf684baa077be465d57497164ec1b6ba3fcaea2
Vitalii <tather13@gmail.com>
2024-07-18 12:39:21 +0300

    receivingData&Async/Await

--- js/script.js
+++ js/script.js
@@ -194,32 +194,37 @@ window.addEventListener('DOMContentLoaded', function () {
     return await res.json();
   };
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render();
+  getResource('http://localhost:3000/menu')
+    .then(data => {
+      data.forEach(({ img, altimg, title, descr, price }) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+      });
+    });
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    14,
-    ".menu .container"
-  ).render();
+  // Способ который не будет использовать классы,использвуется в случае если нужно что=то построить 1 раз
+  // getResource('http://localhost:3000/menu')
+  //   .then(data => createCard(data));
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    ".menu .container"
-  ).render();
+  // function createCard(data) {
+  //   data.forEach(({ img, altimg, title, descr, price }) => {
+  //     const element = document.createElement('div');
+
+  //     element.classList.add('menu__item');
+
+  //     element.innerHTML = `
+  //     <img src=${img} alt=${altimg}>
+  //               <h3 class="menu__item-subtitle">${title}</h3>
+  //               <div class="menu__item-descr">${descr}</div>
+  //               <div class="menu__item-divider"></div>
+  //               <div class="menu__item-price">
+  //                   <div class="menu__item-cost">Цена:</div>
+  //                   <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //               </div>
+  //          `;
+
+  //     document.querySelector('.menu .container').append(element)
+  //   });
+  // }
 
   // Forms
 

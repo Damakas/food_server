3238999fce523221c12dbceb7fcb6b7092d2ae0e
Vitalii <tather13@gmail.com>
2024-07-20 10:43:40 +0300

    creatingSliderPart2BetterSlider

--- js/script.js
+++ js/script.js
@@ -319,56 +319,123 @@ window.addEventListener('DOMContentLoaded', function () {
 
   // Slider
 
-  let slidesIndex = 1;
-
   const slides = document.querySelectorAll('.offer__slide'),
     prev = document.querySelector('.offer__slider-prev'),
     next = document.querySelector('.offer__slider-next'),
     total = document.querySelector('#total'),
-    current = document.querySelector('#current');
+    current = document.querySelector('#current'),
+    slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+    slidesField = document.querySelector('.offer__slider-inner'),
+    width = window.getComputedStyle(slidesWrapper).width;
 
-  showSlides(slidesIndex)
+  let slidesIndex = 1;
+  let offset = 0;
 
   if (slides.length < 10) {
-    total.textContent = `0${slides.length}`
+    total.textContent = `0${slides.length}`;
+    current.textContent = `0${slidesIndex}`;
   } else {
     total.textContent = slides.length
+    current.textContent = slidesIndex;
+
   }
 
-  function showSlides(n) {
-    if (n > slides.length) {
-      slidesIndex = 1
-    }
-    if (n < 1) {
-      slidesIndex = slides.length
-    }
+  slidesField.style.width = 100 * slides.length + '%';
+  slidesField.style.display = 'flex';
+  slidesField.style.transition = '0.5s all';
 
-    slides.forEach(item => {
-      item.classList.add('hide')
-      item.classList.remove('show')
-    })
+  slidesWrapper.style.overflow = 'hidden';
 
-    slides[slidesIndex - 1].classList.add('show')
-    slides[slidesIndex - 1].classList.remove('hide')
-
-    if (slides.length < 10) {
-      current.textContent = `0${slidesIndex}`
-    } else {
-      current.textContent = slidesIndex
-    }
-  }
-
-  function plusIndex(n) {
-    showSlides(slidesIndex += n)
-  }
-
-  prev.addEventListener('click', () => {
-    plusIndex(-1)
+  slides.forEach(slide => {
+    slide.style.width = width;
   });
 
   next.addEventListener('click', () => {
-    plusIndex(1)
+    if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+      offset = 0
+    } else {
+      offset += +width.slice(0, width.length - 2)
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slidesIndex == slides.length) {
+      slidesIndex = 1;
+    } else {
+      slidesIndex++;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slidesIndex}`;
+    } else {
+      current.textContent = slidesIndex;
+    }
   })
+
+  prev.addEventListener('click', () => {
+    if (offset == 0) {
+      offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+    } else {
+      offset -= +width.slice(0, width.length - 2)
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+
+    if (slidesIndex = 1) {
+      slidesIndex = slides.length;
+    } else {
+      slidesIndex--;
+    }
+
+    if (slides.length < 10) {
+      current.textContent = `0${slidesIndex}`;
+    } else {
+      current.textContent = slidesIndex;
+    }
+  })
+
+  // showSlides(slidesIndex)
+
+  // if (slides.length < 10) {
+  //   total.textContent = `0${slides.length}`
+  // } else {
+  //   total.textContent = slides.length
+  // }
+
+  // function showSlides(n) {
+  //   if (n > slides.length) {
+  //     slidesIndex = 1
+  //   }
+  //   if (n < 1) {
+  //     slidesIndex = slides.length
+  //   }
+
+  //   slides.forEach(item => {
+  //     item.classList.add('hide')
+  //     item.classList.remove('show')
+  //   })
+
+  //   slides[slidesIndex - 1].classList.add('show')
+  //   slides[slidesIndex - 1].classList.remove('hide')
+
+  //   if (slides.length < 10) {
+  //     current.textContent = `0${slidesIndex}`
+  //   } else {
+  //     current.textContent = slidesIndex
+  //   }
+  // }
+
+  // function plusIndex(n) {
+  //   showSlides(slidesIndex += n)
+  // }
+
+  // prev.addEventListener('click', () => {
+  //   plusIndex(-1)
+  // });
+
+  // next.addEventListener('click', () => {
+  //   plusIndex(1)
+  // })
 
 
 
